722fd30fd7e648be968516b69fa5a831470153a5
dkuchynskyi <kgdmon@gmail.com>
2018-02-15 16:07:42 +0200

    images

---
+++ src/image.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.sendfile('src/html/image.html');
+});
+
+// Register "connection" events to the WebSocket
+io.on("connection", function(socket) {
+    // Register "join" events, requested by a connected client
+    socket.on("join", function (room) {
+        console.log('Client connected!')
+        // join channel provided by client
+        socket.join(room)
+        // Register "image" events, sent by the client
+        socket.on("image", function(msg) {
+            // Broadcast the "image" event to all other clients in the room
+            socket.broadcast.to(room).emit("image", msg);
+        });
+    })
+});
+
+http.listen(3008, function() {
+    console.log('listening on *:3008');
+});
